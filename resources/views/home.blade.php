593f30c89725dcdb5ffe522b119a805a69fd5c97
prakashtaz0091 <code.ing2468@gmail.com>
2024-06-02 13:55:54 +0545

    update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,87 @@
+@extends('layout.base')
+
+@section('internal-styles')
+    <style>
+        .password-field {
+            position: relative;
+
+            img {
+                position: absolute;
+                top: 50%;
+                right: 10px;
+                transform: translateY(-50%);
+            }
+        }
+    </style>
+@endsection
+
+@section('main-content')
+    <h1>Task Management System</h1>
+    <p class="lead">A Task Management System is a software application that helps manage tasks, projects, and
+        workflows.</p>
+    <p class="lead">
+
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-light text-dark btn-lg" data-bs-toggle="modal" data-bs-target="#signupForm">
+            Signup
+        </button>
+
+        <!-- Modal -->
+    <div class="modal fade" id="signupForm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="signupFormLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-dark" id="signupFormLabel">
+                        Sign up
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    <form>
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="form-floating mb-3 password-field">
+                            <input type="password" class="form-control" id="confirm_password"
+                                placeholder="Confirm Password">
+                            <label for="confirm_password">Confirm Password</label>
+                            <img src="{{ url('images/eye.gif') }}" alt="" height="40" width="40">
+                            <img class="d-none" src="{{ url('images/eye-close.png') }}" alt="" height="40"
+                                width="40">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </p>
+@endsection
+
+
+@section('internal-scripts')
+    <script>
+        const password = document.querySelector('#password');
+        const confirm_password = document.querySelector('#confirm_password');
+        const eye = document.querySelector('.password-field img');
+        eye.addEventListener('click', () => {
+            if (password.type === 'password' && confirm_password.type === 'password') {
+                password.type = 'text';
+                confirm_password.type = 'text';
+                eye.src = "{{ url('images/eye-close.png') }}";
+            } else {
+                password.type = 'password';
+                confirm_password.type = 'password';
+                eye.src = "{{ url('images/eye.gif') }}";
+            }
+        });
+    </script>
+@endsection
---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Prakash Tajpuriya">
+    <meta name="generator" content=" ">
+    <title>Task Management System</title>
+
+    {{-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/"> --}}
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+    @yield('internal-styles')
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/cover.css') }}" rel="stylesheet">
+</head>
+
+<body class="d-flex h-100 text-center text-white bg-dark">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div>
+                <h3 class="float-md-start mb-0">TMS</h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Contact</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="px-3">
+            @yield('main-content')
+        </main>
+
+        <footer class="mt-auto text-white-50">
+            <p>Platform for managing tasks, by <a href="https://prakashtajpuriya.com.np"
+                    class="text-white">@Prakash_Tajpuriya</a>.</p>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+
+
+    @yield('internal-scripts')
+
+</body>
+
+</html>
